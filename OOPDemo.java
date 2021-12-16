a6cacb32c07fa114198b5b2dd329202cad6f41e1
“shubham767676” <shubhamv.singh06@gmail.com>
2021-12-16 19:34:47 +0530

    First commit

---
+++ OOPDemo.java
@@ -0,0 +1,11 @@
+class OOPDemo {
+
+	public static void main(String[] args) {
+		OOPDemo oopDemo = new OOPDemo();
+		oopDemo.demo();
+	}
+	
+	static void demo() {
+		System.out.println("Hello");
+	}
+}
